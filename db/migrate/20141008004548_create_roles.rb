1cf5c5a3661d5c0d5ad5667302380fd0ccbc9607
Jonathan <lee.jonathanw@gmail.com>
2014-10-14 20:20:20 -0500

    Destroyed model Role

--- db/migrate/20141008004548_create_roles.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoles < ActiveRecord::Migration
-  def change
-    create_table :roles do |t|
-      t.belongs_to :user
-      t.string "Role_Type"
-      t.timestamps
-    end
-  end
-end
