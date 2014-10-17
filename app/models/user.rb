e7ed21931f35d91edca79d72cc4977113c507e82
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-10-17 23:19:16 +0700

    #1 Fix association order

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_many :associations, class_name: 'Association', :foreign_key => 'user_id'
   has_many :target_associations, class_name: 'Association', :foreign_key => 'contact_id'
 
-  has_many :source_contacts, :class_name => "User", :source => :user, :through => :associations
-  has_many :contacts, :class_name => "User", :source => :contact, :through => :target_associations
+  has_many :source_contacts, :class_name => "User", :source => :contact, :through => :associations
+  has_many :contacts, :class_name => "User", :source => :user, :through => :target_associations
 
   accepts_nested_attributes_for :associations
 
