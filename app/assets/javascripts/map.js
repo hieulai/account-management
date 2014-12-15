6a27aea2fc672b1ef4d8ae9071ca2abc479a61a5
Hieu Lai Trung <hieulaitrung@gmail.com>
2014-12-15 07:49:27 +0700

    #4 Fill addresses to fields

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,6 +1,12 @@
 var Map = (function ($) {
 
     var countryRestrict = { 'country': 'us' };
+    var componentForm = {
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        postal_code: 'short_name'
+    };
+
     var init = function () {
         initAutoComplete('[data-name="address"]');
     };
@@ -13,12 +19,29 @@ var Map = (function ($) {
         }
 
         for (var i = 0; i < $addressInputs.length; i++) {
-            new google.maps.places.Autocomplete($addressInputs[i], { types: ['geocode'], componentRestrictions: countryRestrict });
+            var autocomplete = new google.maps.places.Autocomplete($addressInputs[i], { types: ['geocode'], componentRestrictions: countryRestrict });
+            if ( $($addressInputs[i]).data("id") == "main") {
+                google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                    fillInAddress(this);
+                });
+            }
+
             google.maps.event.addDomListener($addressInputs[i], 'keydown', function (e) {
                 if (e.keyCode == 13) {
                     e.preventDefault();
                 }
             });
+        }
+    };
+
+    var fillInAddress = function (autocomplete) {
+        var place = autocomplete.getPlace();
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                $('[data-name="' + addressType + '"]').val(val);
+            }
         }
     };
 
